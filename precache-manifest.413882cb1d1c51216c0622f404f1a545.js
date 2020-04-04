b2bf4cf30caef0e09d99d43d77401cba7a71feb6
Arkadiusz Lis <alis@efigence.com>
2020-04-04 13:56:15 +0200

    Updates

---
+++ precache-manifest.413882cb1d1c51216c0622f404f1a545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1366db7379b5a250e9fd4c794c0959",
+    "url": "/React-Tic_Tac_Toe/index.html"
+  },
+  {
+    "revision": "bb69daa551518d3c9bdb",
+    "url": "/React-Tic_Tac_Toe/static/css/main.9451bbf2.chunk.css"
+  },
+  {
+    "revision": "81d00861ab4a46f37b6e",
+    "url": "/React-Tic_Tac_Toe/static/js/2.9d620a4a.chunk.js"
+  },
+  {
+    "revision": "bb69daa551518d3c9bdb",
+    "url": "/React-Tic_Tac_Toe/static/js/main.0bb62ce7.chunk.js"
+  },
+  {
+    "revision": "9c4ebc87d76afc298289",
+    "url": "/React-Tic_Tac_Toe/static/js/runtime-main.2459b505.js"
+  }
+]);
