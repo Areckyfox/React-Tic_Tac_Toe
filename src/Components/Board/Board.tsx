57a3738b471f280308efea22221a4a747abaf68f
Arkadiusz Lis <alis@efigence.com>
2020-04-22 19:28:20 +0200

    add type script

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -4,9 +4,9 @@ import { createArray } from '../../Helpers/helpers';
 
 interface BoardProps {
   onClicked: (num: number) => void;
-  squares: string | null[];
-  toColor: any;
-}
+  squares: string[];
+  toColor: number[] | null;
+};
 
 const Board: React.FC<BoardProps> = ({ toColor, squares, onClicked }) => {
   const renderSquare = (i: number) => {
@@ -18,17 +18,21 @@ const Board: React.FC<BoardProps> = ({ toColor, squares, onClicked }) => {
       <Square
         key={`square${i}`}
         value={squares[i]}
-        onClick={() => onClicked(i)}
+        onClicked={() => onClicked(i)}
         toColor={toColorSquare}
       />
     );
   };
 
-  return createArray(3).map((i) => (
-    <div key={`row${i}`} className='board-row'>
-      {createArray(3).map((j, index, arr) => renderSquare(j + i * arr.length))}
-    </div>
-  ));
+  return(
+    <>
+     {createArray(3).map((i) => (
+      <div key={`row${i}`} className='board-row'>
+        {createArray(3).map((j, index, arr) => renderSquare(j + i * arr.length))}
+      </div>
+    ))}
+    </>
+  )    
 };
 
 export default Board;
