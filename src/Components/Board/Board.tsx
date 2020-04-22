873a5265a7de6cb03df01150d6cb8b3ee60c4925
Arkadiusz Lis <alis@efigence.com>
2020-04-22 19:20:54 +0200

    format document

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Square from '../Board/Square/Square';
-import {createArray} from '../../Helpers/helpers';
+import { createArray } from '../../Helpers/helpers';
 
 interface BoardProps {
   onClicked: (num: number) => void;
@@ -9,27 +9,26 @@ interface BoardProps {
 }
 
 const Board: React.FC<BoardProps> = ({ toColor, squares, onClicked }) => {
-
-  const renderSquare = (i:number) => {
-    let toColorSquare = "";
-    if (toColor && toColor.some((el:number) => el === i)) {
-      toColorSquare = "toColor";
+  const renderSquare = (i: number) => {
+    let toColorSquare = '';
+    if (toColor && toColor.some((el: number) => el === i)) {
+      toColorSquare = 'toColor';
     }
     return (
       <Square
-        key = {`square${i}`}
+        key={`square${i}`}
         value={squares[i]}
         onClick={() => onClicked(i)}
         toColor={toColorSquare}
       />
     );
   };
-  
-  return createArray(3).map(i => (
-      <div key={`row${i}`} className="board-row">
-          {createArray(3).map((j,index, arr) => renderSquare(j +(i * arr.length)))}
-      </div>    
-  ))
+
+  return createArray(3).map((i) => (
+    <div key={`row${i}`} className='board-row'>
+      {createArray(3).map((j, index, arr) => renderSquare(j + i * arr.length))}
+    </div>
+  ));
 };
 
 export default Board;
